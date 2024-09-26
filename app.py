1a2c37fd223615940c270bdae88ac447e4fc294c
Aryan Tiwari <21f1001076@ds.study.iitm.ac.in>
2024-09-26 22:23:55 +0530

    Update requirements.txt and app.py
    - Update tensorflow to tensorflow-cpu in requirements.txt
    - Add code to app.py to simplify gender encoding
    - Reshape input data for prediction consistency in app.py
    - Add logging configuration to app.py
    - Remove unused code from prerdictions.ipynb

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from tensorflow.keras.models import load_model # type: ignore
 import streamlit as st
 import dill
 
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import absl.logging
+absl.logging.set_verbosity(absl.logging.ERROR)
+
 
 # loading all models
 with open("ohe.pkl", "rb") as file:
@@ -49,18 +54,25 @@ idata = {
 df1 = pd.DataFrame([idata.values()], columns=idata.keys())
 
 
+# Gender encoding
 def preprocess(id):
-    id["Gender"] = ge(id)
-    id1 = pd.DataFrame(ohe.transform(id["Geography"].values.reshape(
-        -1, 1)).toarray(), columns=ohe.get_feature_names_out(['Geography']))
+    id["Gender"] = id["Gender"].map({"Male": 1, "Female": 0})  # Simplified gender encoding
+    id1 = pd.DataFrame(ohe.transform(id["Geography"].values.reshape(-1, 1)).toarray(), 
+                       columns=ohe.get_feature_names_out(['Geography']))
     id.drop("Geography", inplace=True, axis=1)
     id = pd.concat([id, id1], axis=1)
     id = sc.transform(id)
     return id
 
 
+
+# Ensuring consistent input shape for prediction
 df1 = preprocess(df1)
+df1 = np.array(df1).reshape(1, -1)  # Reshaping for batch consistency
+
+# Predict outside any loop and avoid retracing
 pred = model.predict(df1)
+
 
 st.write(f"Churn Probability :{pred[0][0]:.2f}")
 if pred[0][0] > 0.5:
